3ba382b5433d0fef717f4cb5426218437b319d21
Ajeet jha <jhaajeetcs@gmail.com>
2021-06-07 14:25:49 +0530

    basic sets operations

---
+++ set_operations/index.js
@@ -0,0 +1,45 @@
+class SetOperation {
+  constructor(arr1, arr2) {
+    (this.arr1 = arr1), (this.arr2 = arr2);
+  }
+
+  //union
+  union() {
+    return [...new Set(this.arr1.concat(this.arr2))];
+  }
+
+  //intersection
+  intersection() {
+    return this.arr1.filter((ele) => this.arr2.includes(ele));
+  }
+
+  //Difference
+  difference() {
+    return this.arr1.filter((ele) => !this.arr2.includes(ele));
+  }
+
+  //symentic Difference
+  symenticDifference() {
+    return this.arr1
+      .filter((ele) => !this.arr2.includes(ele))
+      .concat(this.arr2.filter((ele) => !this.arr1.includes(ele)));
+  }
+}
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr2 = [2, 4, 6, 8, 10, 11];
+
+const elements = new SetOperation(arr1,arr2);
+console.log({
+"Union" : elements.union()
+});
+console.log({
+   "Intersection" : elements.intersection()
+});
+console.log({
+    "difference" : elements.difference()
+});
+console.log({
+    "symenticDifference" : elements.symenticDifference()
+});
+
